1fa40e42ca91e4214c31ab11cff183e7d4758c8a
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-07-17 13:12:50 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import pyautogui
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+pcap = pyautogui.prompt("Enter path to pcap: ")
+val = "sudo tcpreplay -i eth1 " + pcap
+stream = os.popen(val
+out = stream.read()
+pyautogui.alert(out)
