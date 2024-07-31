50612cf08948dfdb45b12d68829c3835047bb350
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-07-31 11:48:59 +0100

    fix: declare path

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+	path = None
 	if request.method == "POST":
 		filepath = request.form["filepath"]
 	return render_template("index.html", path=path)
