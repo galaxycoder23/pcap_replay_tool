8f0b840f53afb8e835f5ebf66caeed8d5ae07d19
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-01 01:34:24 +0100

    feat: add command implementation

--- streamlit_test.py
+++ streamlit_test.py
@@ -13,4 +13,6 @@ st.subheader("PCAP replay speed (in pps)")
 replay_speed = st.slider("How many packets would you like to replay per second?", 0.25, 200.0, 25.0)
 
 command = "sudo tcpreplay -i eth1 " + "-p " + str(replay_speed) + " " + filename
-
+stream = os.popen(val)
+out = stream.read()
+st.write(out)
