8a8d591fc78815c79b1c49ca4214d8f2f13d472c
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-01 10:48:17 +0100

    chore: update pcap_replay_tool.py

--- pcap_replay_tool.py
+++ pcap_replay_tool.py
@@ -37,6 +37,6 @@ if st.button("Start PCAP replay"):
 	st.write(out)
 
 if st.button("Stop PCAP replay", type="primary"):
-	stream2 = os.popen("ps aux | grep \"tcpreplay\"")
+	stream2 = os.popen(SIGINT)
 	out2 = stream2.read()
 	st.write(out2)
