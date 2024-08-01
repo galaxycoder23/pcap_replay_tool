0a73c707a9af6602b0b1840a1f9d8b646abacd26
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-01 01:34:55 +0100

    refactor: update mis-matched variable names

--- streamlit_test.py
+++ streamlit_test.py
@@ -13,6 +13,6 @@ st.subheader("PCAP replay speed (in pps)")
 replay_speed = st.slider("How many packets would you like to replay per second?", 0.25, 200.0, 25.0)
 
 command = "sudo tcpreplay -i eth1 " + "-p " + str(replay_speed) + " " + filename
-stream = os.popen(val)
+stream = os.popen(command)
 out = stream.read()
 st.write(out)
