3908c76bdc0591ebb921d50f1ef4675a4bedc9e1
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-01 01:31:04 +0100

    fix: cast replay_speed to string for command

--- streamlit_test.py
+++ streamlit_test.py
@@ -12,5 +12,5 @@ filename = file_selector()
 st.subheader("PCAP replay speed (in pps)")
 replay_speed = st.slider("How many packets would you like to replay per second?", 0.25, 200.0, 25.0)
 
-command = "sudo tcpreplay -i eth1 " + "-p " + replay_speed + " " + filename
+command = "sudo tcpreplay -i eth1 " + "-p " + str(replay_speed) + " " + filename
 
