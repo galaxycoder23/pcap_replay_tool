794a97362f91045bfcd2568ee6b9c1fb0cdde768
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-05 22:17:30 +0100

    chore: update remote path

--- pcap_replay_tool.py
+++ pcap_replay_tool.py
@@ -27,7 +27,7 @@ if file_upload:
 	filename = file_upload.name
 	with open(filename, 'wb') as f: 
 		f.write(file_upload.getvalue()) # For uploaded file as bytes
-	remote_path = "~/packet_captures/" + filename
+	remote_path = "~/Documents/packet_captures/" + filename
 
 
 # Adjust replay speed
