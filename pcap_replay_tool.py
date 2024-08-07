feb617feeee980099ee6238a12343687506de7ac
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-07 21:05:56 +0100

    chore: read file

--- pcap_replay_tool.py
+++ pcap_replay_tool.py
@@ -51,7 +51,8 @@ if file_uploads:
 	with open(filename, 'wb') as f: 
 		for uploaded_file in file_uploads:
 			f.write(uploaded_file.getvalue()) # For uploaded file as bytes
-		st.download_button("Download PCAP", f)
+	with open(filename) as saved_file:
+		st.download_button("Download PCAP", saved_file)
 	remote_path = "~/Documents/packet_captures/" + filename
 
 	
