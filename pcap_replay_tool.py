fbb8e3cc9e61ce18b66338a757d6f7ab98584c13
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-07 21:14:51 +0100

    chore: update mime

--- pcap_replay_tool.py
+++ pcap_replay_tool.py
@@ -52,7 +52,7 @@ if file_uploads:
 		for uploaded_file in file_uploads:
 			f.write(uploaded_file.getvalue()) # For uploaded file as bytes
 	with open(filename, "rb") as saved_file:
-		st.download_button("Download PCAP", saved_file)
+		st.download_button("Download PCAP", saved_file, mime="application/vnd.tcpdump.pcap")
 	remote_path = "~/Documents/packet_captures/" + filename
 
 	
