7e88804443f067a3ae8b82f4265137c71ee73af5
galaxycoder23 <145396594+galaxycoder23@users.noreply.github.com>
2024-08-08 10:41:21 +0100

    feat: add delete button

--- pcap_replay_tool.py
+++ pcap_replay_tool.py
@@ -38,24 +38,29 @@ def display_command():
 		replay_command = "sudo -S tcpreplay -i eth1 -vv " + "-p " + str(replay_speed) + " " + remote_path
 		st.code(replay_command, language="bash")
 
-	
+def delete_file():
+	if os.path.exists("./upload_folder/"):
+			os.remove("./upload_folder/")
 		
 file_uploads = st.file_uploader("Upload files", type=(["pcap"]), accept_multiple_files=True, on_change=display_command)
 if file_uploads:
+	if not(os.path.exists("./upload_folder/")):
+		os.mkdir("./upload_folder/")
 	if len(file_uploads) == 1:
 		filename = file_uploads[0].name
-		with open(filename, "wb") as f: 
+		with open("./upload_folder/"+filename, "wb") as f: 
 			for uploaded_file in file_uploads:
 				f.write(uploaded_file.getvalue()) # For uploaded file as bytes
 				st.download_button("Download PCAP", uploaded_file.getvalue(), file_name=filename, mime="application/vnd.tcpdump.pcap")
 	else:
 		for uploaded_file in file_uploads:
 			filename = "mergedpcap_" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".pcap"
-		with open(filename, "wb") as f: 
+		with open("./upload_folder/"+filename, "wb") as f: 
 			for uploaded_file in file_uploads:
 				f.write(uploaded_file.getvalue()) # For uploaded file as bytes
-		with open(filename, "rb") as merged_file:
+		with open("./upload_folder/"+filename, "rb") as merged_file:
 			st.download_button("Download merged PCAP", merged_file, file_name=filename, mime="application/vnd.tcpdump.pcap")
+	st.button("Delete all uploaded PCAPs", on_click=delete_file)
 	remote_path = "~/Documents/packet_captures/" + filename
 
 	
@@ -115,8 +120,7 @@ def replay_traffic(ssh):
 		
 		traffic_replay = stdout.channel
 	finally:
-		if os.path.exists(filename):
-			os.remove(filename)
+		delete_file()
 		ssh.close()
 
 if st.button("Start traffic replay"):
@@ -128,8 +132,7 @@ def stop_traffic(ssh):
 	ssh.connect(ssh_host, port=ssh_port, username=ssh_user, password=ssh_password)
 	if "traffic_replay" in globals():
 		ssh.exec_command(f"sudo kill {traffic_replay.get_id()}")
-	if os.path.exists(filename):
-		os.remove(filename)
+	delete_file()
 	ssh.close()
 
 if st.button("Stop traffic replay", type="primary"):
